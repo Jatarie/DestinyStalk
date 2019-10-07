3b050a0a4ae216bd6319c8e87abe9344ec99246d
matthew <matthew.e.j.smyth@gmail.com>
2019-10-07 15:37:11 +0100

    deleted more l

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import XP from './Components/Graph/XP';
-import LCategories from './Components/Literotica/LCategories';
-import LCatStories from './Components/Literotica/LCatStories';
-import LStory from './Components/Literotica/LStory';
 import Stalk from './Components/Stalk/Stalk';
 import Crafting from './Components/Crafting/Crafting';
 import { HashRouter, Route } from 'react-router-dom';
@@ -22,9 +19,6 @@ class App extends Component {
           <div>
             <Route path="/stalk" component={Stalk} />
             <Route path="/graph" component={XP} />
-            <Route path="/literotica/c/:category" exact component={LCatStories} />
-            <Route path="/literotica/s/:story" exact component={LStory} />
-            <Route path="/literotica" exact component={LCategories} />
             <Route path="/crafting" exact component={Crafting} />
           </div>
         </div>
