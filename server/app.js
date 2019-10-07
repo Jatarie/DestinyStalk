b3e5b85578a6f669e08c2aa93c9005bfeed8a401
matthew <matthew.e.j.smyth@gmail.com>
2019-10-07 15:35:25 +0100

    deleted l

--- server/app.js
+++ server/app.js
@@ -2,12 +2,12 @@
 const express = require('express')
 const app = express();
 
-var literoticaRouter = require('./routes/literotica/literotica');
+
 var stalkRouter = require('./routes/stalk/stalk');
 var craftingRouter = require('./routes/crafting/crafting');
 var graphRouter = require('./routes/graph/graph');
 
-app.use("/literotica", literoticaRouter)
+
 app.use("/api", stalkRouter)
 app.use("/crafting", craftingRouter)
 app.use("/graphapi", graphRouter)
